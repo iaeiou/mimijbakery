ab664e0da6003fb352d7181595f9221c8ca8e524
bret <bbeatty14@gmail.com>
2021-12-11 20:12:39 -0600

    Added navbar

---
+++ static/js/main.js
@@ -0,0 +1,9 @@
+const load = () => {
+    console.log("Load event occurred");
+}
+
+window.onload = load;
+
+console.log("Javascript loaded");
+
+console.log("This is Mimi J's website");
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,11 @@
 module.exports = {
   content: ['layouts/**/*.html'],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        'corinth': ['corinthia', 'cursive']
+      },
+    },
   },
   plugins: [],
 }
